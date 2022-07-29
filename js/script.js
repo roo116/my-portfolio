26f420dca10f798d93df8e114cd3b8fb7b21043d
Chip Hill <hillkarl03@gmail.com>
2022-07-29 17:26:42 -0400

    styling of header and nav bar

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// when about me is clicked then the GSAP animation takes place
+// $("#about").click(function () {
+//   gsap.from("#about-me", 1, { y: -100, scale: 0.85, delay: 1 });
+//   gsap.to("#about-me", 1, { opacity: 1, delay: 1 });
+// $('#about-me').show
+
+// });
+
+// on click, hide About Me
+// $(document).click(function(){$('#about-me').hide();});
+
+// function animateAboutMe() {
+
+// }
+function showAboutMe () {
+  
+}
+
+
+$("#about").click(function () {
+  console.log("you clicked about me button")
+  console.log($("#about-me"));
+});
