28262ebb8a30360a7148b1644114c4f1c7145de8
Chip Hill <hillkarl03@gmail.com>
2022-10-08 16:46:57 -0400

    set sections to fade in rather than swing in

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,26 +11,26 @@ $("#about").click(function () {
   $("#resume-section").hide();
   $("#projects").hide();
   $("#contact-section").hide();
-  $("#about-section").toggle("fast");
+  $("#about-section").fadeIn("fast");
 });
 
 $("#project").click(function () {
   $("#about-section").hide();
   $("#contact-section").hide();
   $("#resume-section").hide();
-  $("#projects").toggle("fast");
+  $("#projects").fadeIn("fast");
 });
 
 $("#contact").click(function () {
   $("#about-section").hide();
   $("#projects").hide();
   $("#resume-section").hide();
-  $("#contact-section").toggle("fast");
+  $("#contact-section").fadeIn("fast");
 });
 
 $("#resume").click(function () {
   $("#about-section").hide();
   $("#projects").hide();
   $("#contact-section").hide();
-  $("#resume-section").toggle("fast");
+  $("#resume-section").fadeIn("fast");
 });
