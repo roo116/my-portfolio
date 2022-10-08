dbbc1b0ee726487edeb13ef03fb485b872348428
Chip Hill <hillkarl03@gmail.com>
2022-10-08 16:29:31 -0400

    added contacts and resume sections, enabled some of the javascript functions, cleaned up some styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,36 @@
+// JQuery dom manipulation
 
 $(document).ready(function () {
   $("#about-section").hide();
   $("#projects").hide();
+  $("#contact-section").hide();
+  $("#resume-section").hide();
 });
 
 $("#about").click(function () {
+  $("#resume-section").hide();
   $("#projects").hide();
+  $("#contact-section").hide();
   $("#about-section").toggle("fast");
 });
 
 $("#project").click(function () {
   $("#about-section").hide();
+  $("#contact-section").hide();
+  $("#resume-section").hide();
   $("#projects").toggle("fast");
+});
+
+$("#contact").click(function () {
+  $("#about-section").hide();
+  $("#projects").hide();
+  $("#resume-section").hide();
+  $("#contact-section").toggle("fast");
+});
+
+$("#resume").click(function () {
+  $("#about-section").hide();
+  $("#projects").hide();
+  $("#contact-section").hide();
+  $("#resume-section").toggle("fast");
 });
